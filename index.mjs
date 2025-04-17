7983963ac086e7fbfdfe2c68836fa3c2be812dd0
Denys D <ouctek786@gmail.com>
2025-04-17 21:23:42 +0300

    bot

--- index.mjs
+++ index.mjs
@@ -15,7 +15,6 @@ const categories = {
 async function searchOLX(query, minPrice, maxPrice) {
   const browser = await puppeteer.launch({
   headless: true,
-  executablePath: '/usr/bin/chromium-browser',
   args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
